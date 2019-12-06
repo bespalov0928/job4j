2319518c73e31fcf2bc9723b79f42688f50ca23c
Dmitry Pratsun <pratsundv@gmail.com>
2019-12-06 22:24:07 +0200

    Additional tests have been added

--- chapter_001/src/test/java/ru/job4j/condition/MultiMaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MultiMaxTest.java
@@ -12,4 +12,18 @@ public class MultiMaxTest {
         int result = check.max(1, 4, 2);
         assertThat(result, is(4));
     }
+
+    @Test
+    public void whenFirstMax() {
+        MultiMax check = new MultiMax();
+        int result = check.max(4, 1, 2);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenThirdMax() {
+        MultiMax check = new MultiMax();
+        int result = check.max(2, 1, 4);
+        assertThat(result, is(4));
+    }
 }
