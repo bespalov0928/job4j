8427584ec0fc122c38b28ccf68ba608df12db3b8
Dmitry Pratsun <pratsundv@gmail.com>
2019-12-08 13:04:22 +0200

    EndsWith class have been added

---
+++ chapter_001/src/test/java/ru/job4j/array/EndsWithTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class EndsWithTest {
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'l', 'o'};
+        boolean result = EndsWith.endsWith(word, post);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenNotStartWithPrefixThenFalse() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'l', 'a'};
+        boolean result = EndsWith.endsWith(word, post);
+        assertThat(result, is(false));
+    }
+}
