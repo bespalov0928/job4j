35fb9aceeb483ad15cc1b31f41f3f77f739e5310
Dmitry Pratsun <pratsundv@gmail.com>
2019-12-08 12:12:34 +0200

    Turn class have been moved to array package

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -1,4 +1,4 @@
-package ru.job4j.loop;
+package ru.job4j.array;
 
 public class Turn {
     public int[] back(int[] array) {
--- chapter_001/src/test/java/ru/job4j/array/TurnTest.java
+++ chapter_001/src/test/java/ru/job4j/array/TurnTest.java
@@ -1,6 +1,7 @@
-package ru.job4j.loop;
+package ru.job4j.array;
 
 import org.junit.Test;
+import ru.job4j.array.Turn;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
