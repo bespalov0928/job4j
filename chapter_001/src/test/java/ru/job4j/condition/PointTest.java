11ee32f6e16da7484890a0d52ea79545bd030018
Dmitry Pratsun <pratsundv@gmail.com>
2019-12-10 18:01:50 +0200

    Point class have been refactored

--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.condition;
 
+import org.junit.Ignore;
 import ru.job4j.condition.Point;
 import org.junit.Assert;
 import org.junit.Test;
@@ -8,12 +9,10 @@ public class PointTest {
 
     @Test
     public void testDistance() {
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double result = a.distance(b);
         double expected = 2;
-        double result = Point.distance(x1, y1, x2, y2);
 
         Assert.assertEquals(expected, result, 0.01);
     }
