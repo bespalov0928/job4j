5f3999d3a18faf2041f3db5af0f263c29b9d0115
Dmitry Pratsun <pratsundv@gmail.com>
2019-12-10 19:27:00 +0200

    Triangle class have been refactored

--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -3,12 +3,49 @@ package ru.job4j.condition;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class TriangleTest {
     @Test
     public void whenExist() {
-        boolean result = Triangle.exist(2.0, 2.0, 2.0);
+        Point a = new Point(2, 2);
+        Point b = new Point(4, 5);
+        Point c = new Point(7, 1);
+        Triangle triangle = new Triangle(a, b, c);
+        boolean result = triangle.exist(a.distance(b), a.distance(c), b.distance(c));
         assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenNotExist() {
+        Point a = new Point(2, 2);
+        Point b = new Point(2, 2);
+        Point c = new Point(7, 1);
+        Triangle triangle = new Triangle(a, b, c);
+        boolean result = triangle.exist(a.distance(b), a.distance(c), b.distance(c));
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void testPeriod() {
+        Point a = new Point(2, 2);
+        Point b = new Point(4, 5);
+        Point c = new Point(7, 1);
+        Triangle triangle = new Triangle(a, b, c);
+        double result = triangle.period(a.distance(b), a.distance(c), b.distance(c));
+        double expected = 6.8;
+        assertEquals(expected, result, 0.1);
+    }
+
+    @Test
+    public void testArea() {
+        Point a = new Point(2, 2);
+        Point b = new Point(4, 5);
+        Point c = new Point(7, 1);
+        Triangle triangle = new Triangle(a, b, c);
+        double result = triangle.area();
+        double expected = 8.5;
+        assertEquals(expected, result, 0.1);
     }
 }
