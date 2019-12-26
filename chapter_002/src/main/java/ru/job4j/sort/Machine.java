cf801ff3a4fde01f4dd6c60343482bfdbe117f4b
Dmitry Pratsun <pratsundv@gmail.com>
2019-12-26 08:22:17 +0200

    Сдача в кофе машине. [#34741]

---
+++ chapter_002/src/main/java/ru/job4j/sort/Machine.java
@@ -0,0 +1,20 @@
+package ru.job4j.sort;
+
+import java.util.Arrays;
+
+public class Machine {
+    private final int[] coins = {10, 5, 2, 1};
+
+    public int[] change(int money, int price) {
+        int[] rsl = new int[100];
+        int size = 0;
+        int change = money - price;
+        for (int index = 0; index < coins.length && change > 0; index++) {
+            while ((change - coins[index]) >= 0) {
+                rsl[size++] = coins[index];
+                change -= coins[index];
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+}
