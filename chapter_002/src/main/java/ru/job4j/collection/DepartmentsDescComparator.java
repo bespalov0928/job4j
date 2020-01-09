8d92f4b9f169965d1b77dc039963703850c48b5b
Dmitry Pratsun <pratsundv@gmail.com>
2020-01-09 21:15:48 +0200

    Отсортировать департаменты [#34352] Убран лишний цикл

--- chapter_002/src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
+++ chapter_002/src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -5,12 +5,11 @@ import java.util.Comparator;
 public class DepartmentsDescComparator implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        String[] splitO1 = o1.split("/");
-        String[] splitO2 = o2.split("/");
-        int result = 0;
-        for (int i = 0; i < splitO1.length && i < splitO2.length; i++) {
-            result = splitO2[i].compareTo(splitO1[i]);
-            if (result != 0) {
+        int result = o1.length() - o2.length();
+        int length = Math.min(o1.length(), o2.length());
+        for (int i = 0; i < length; i++) {
+            if (o1.charAt(i) != o2.charAt(i)) {
+                result = o2.charAt(i) - o1.charAt(i);
                 break;
             }
         }
