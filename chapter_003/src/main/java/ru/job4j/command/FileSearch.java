d1c7fe39e17f31021f5bc9e8a248519903d3ed4c
Dmitry Pratsun <pratsundv@gmail.com>
2020-03-07 20:55:18 +0200

    FileSearch have been refactored. Tests have been added.

--- chapter_003/src/main/java/ru/job4j/command/FileSearch.java
+++ chapter_003/src/main/java/ru/job4j/command/FileSearch.java
@@ -8,6 +8,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 public class FileSearch {
     private static final String DIRECTORY_OPTION = "-d";
@@ -16,9 +17,10 @@ public class FileSearch {
     private static final String FILENAME_FULL_OPTION = "-f";
     private static final String OUTPUT_OPTION = "-o";
 
-    public static void main(String[] args) {
-        CommandOptions commandOptions = new CommandOptions();
+    private final CommandOptions commandOptions = new CommandOptions();
+    private final ChainedFileFilter fileFilter = new ChainedFileFilter();
 
+    private void setCommandOptions() {
         commandOptions.add(new CommandOption(
                 DIRECTORY_OPTION,
                 "directory for searching",
@@ -46,15 +48,9 @@ public class FileSearch {
                 FILENAME_FULL_OPTION,
                 "search file by name",
                 false));
+    }
 
-        if (!commandOptions.parse(args)) {
-            commandOptions.printErrors();
-            commandOptions.printHelp();
-            return;
-        }
-
-        ChainedFileFilter fileFilter = new ChainedFileFilter();
-
+    private void setFileFilters() {
         fileFilter.add(File::isDirectory);
         var fileName = commandOptions.getOptionValue(FILENAME_OPTION);
         if (commandOptions.getOption(FILENAME_FULL_OPTION).isPresent()) {
@@ -64,13 +60,40 @@ public class FileSearch {
         } else {
             fileFilter.add(f -> fileName.equals(f.getName()));
         }
+    }
 
-        var files = new ru.job4j.io.FileSearch().files(commandOptions.getOptionValue(DIRECTORY_OPTION), fileFilter);
+    private boolean parseArgs(String[] args) {
+        var result = commandOptions.parse(args);
+        if (!result) {
+            commandOptions.printErrors();
+            commandOptions.printHelp();
+        }
+        return result;
+    }
 
-        try (PrintWriter pw = new PrintWriter(new FileWriter(commandOptions.getOptionValue(OUTPUT_OPTION)))) {
-            files.forEach(file -> pw.println(file.getAbsolutePath()));
+    private List<File> searchFiles() {
+        return new ru.job4j.io.FileSearch().files(commandOptions.getOptionValue(DIRECTORY_OPTION), fileFilter);
+    }
+
+    private void saveResultToFile(List<File> files) {
+        try (var pw = new PrintWriter(new FileWriter(commandOptions.getOptionValue(OUTPUT_OPTION)))) {
+            files.stream().map(File::getAbsolutePath).forEach(pw::println);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void search(String[] args) {
+        setCommandOptions();
+        if (!parseArgs(args)) {
+            return;
+        }
+        setFileFilters();
+        var files = searchFiles();
+        saveResultToFile(files);
+    }
+
+    public static void main(String[] args) {
+        new FileSearch().search(args);
     }
 }
---
+++ chapter_003/src/test/java/ru/job4j/command/FileSearchTest.java
@@ -0,0 +1,74 @@
+package ru.job4j.command;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.*;
+
+import static org.junit.Assert.*;
+
+public class FileSearchTest {
+    private final String path = System.getProperty("java.io.tmpdir") + "filesearchtest";
+    private final String resultFile = path + "/log.txt";
+
+    @Before
+    public void setUp() {
+        new File(path + "/1/2/3").mkdirs();
+        try {
+            new File(path + "/1/1.txt").createNewFile();
+            new File(path + "/1/2/2.csv").createNewFile();
+            new File(path + "/1/2/3/3.xml").createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void whenSearchCommandExecutedThanResultFileShouldAppear() {
+        String[] args = {"-d", path, "-n", "*.pdf", "-m", "-o", resultFile};
+        new FileSearch().search(args);
+        assertTrue(new File(resultFile).exists());
+    }
+
+    @Test
+    public void whenFilesNotFoundThanResultFileShouldBeEmpty() {
+        String[] args = {"-d", path, "-n", "*.pdf", "-m", "-o", resultFile};
+        new FileSearch().search(args);
+        assertEquals("", getFileContent());
+    }
+
+    @Test
+    public void whenFileIsFoundThanResultFileShouldContainFilePath() {
+        String[] args = {"-d", path, "-n", "3.xml", "-m", "-o", resultFile};
+        new FileSearch().search(args);
+        var resultFilePath = path + "/1/2/3/3.xml";
+        assertEquals(resultFilePath, getFileContent());
+    }
+
+    @Test
+    public void whenArgumentInputIsWrongThanResultFileShouldNotAppear() {
+        String[] args = {"-d", path, "-n", "*.pdf", "-m", "-o"};
+        new FileSearch().search(args);
+        assertFalse(new File(resultFile).exists());
+    }
+
+    @After
+    public void tearDown() {
+        var resultFile = new File(this.resultFile);
+        if (resultFile.exists()) {
+            resultFile.delete();
+        }
+    }
+
+    private String getFileContent() {
+        var resultFile = new File(this.resultFile);
+        var result = "";
+        try (BufferedReader bf = new BufferedReader(new FileReader(resultFile))) {
+            result = bf.lines().reduce("", String::concat);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
