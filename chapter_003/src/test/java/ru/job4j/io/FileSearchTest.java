aeb270acef9f29917175f8075490b8db2bfbe060
Dmitry Pratsun <pratsundv@gmail.com>
2020-03-19 15:05:01 +0200

    FileSearchTest change for passing Travis build

--- chapter_003/src/test/java/ru/job4j/io/FileSearchTest.java
+++ chapter_003/src/test/java/ru/job4j/io/FileSearchTest.java
@@ -12,15 +12,15 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class FileSearchTest {
-    private String path = System.getProperty("java.io.tmpdir") + "filesearchtest" + File.separator;
+    private static final String SYSTEM_TMP = System.getProperty("java.io.tmpdir");
 
     @Before
     public void setUp() {
-        new File(path + String.join(File.separator, "1", "2", "3")).mkdirs();
+        new File(SYSTEM_TMP + String.join(File.separator, "1", "2", "3")).mkdirs();
         try {
-            new File(path + "1" + File.separator + "1.txt").createNewFile();
-            new File(path + String.join(File.separator, "1", "2", "2.csv")).createNewFile();
-            new File(path + String.join(File.separator, "1", "2", "3", "3.xml")).createNewFile();
+            new File(SYSTEM_TMP + "1" + File.separator + "1.txt").createNewFile();
+            new File(SYSTEM_TMP + String.join(File.separator, "1", "2", "2.csv")).createNewFile();
+            new File(SYSTEM_TMP + String.join(File.separator, "1", "2", "3", "3.xml")).createNewFile();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -28,7 +28,7 @@ public class FileSearchTest {
 
     @Test
     public void whenReturnOnlyTxtFile() {
-        List<File> result = new FileSearch().files(path, List.of("txt"));
+        List<File> result = new FileSearch().files(SYSTEM_TMP, List.of("txt"));
         assertThat(
                 result.stream().map(File::getName).collect(Collectors.joining()),
                 is("1.txt")
@@ -39,7 +39,7 @@ public class FileSearchTest {
         chainedFilter.add(File::isDirectory);
         chainedFilter.add(new ExtensionFileFilter("txt"));
 
-        result = new FileSearch().files(path, chainedFilter);
+        result = new FileSearch().files(SYSTEM_TMP, chainedFilter);
         assertThat(
                 result.stream().map(File::getName).collect(Collectors.joining()),
                 is("1.txt")
@@ -48,7 +48,7 @@ public class FileSearchTest {
 
     @Test
     public void whenReturnOnlyCsvFile() {
-        List<File> result = new FileSearch().files(path, List.of("csv"));
+        List<File> result = new FileSearch().files(SYSTEM_TMP, List.of("csv"));
         assertThat(
                 result.stream().map(File::getName).collect(Collectors.joining()),
                 is("2.csv")
@@ -59,7 +59,7 @@ public class FileSearchTest {
         chainedFilter.add(File::isDirectory);
         chainedFilter.add(new ExtensionFileFilter("csv"));
 
-        result = new FileSearch().files(path, chainedFilter);
+        result = new FileSearch().files(SYSTEM_TMP, chainedFilter);
         assertThat(
                 result.stream().map(File::getName).collect(Collectors.joining()),
                 is("2.csv")
@@ -68,7 +68,7 @@ public class FileSearchTest {
 
     @Test
     public void whenReturnAllFiles() {
-        List<File> result = new FileSearch().files(path, List.of("txt", "csv", "xml"));
+        List<File> result = new FileSearch().files(SYSTEM_TMP, List.of("txt", "csv", "xml"));
         assertThat(
                 result.stream().map(File::getName).collect(Collectors.toList()),
                 is(List.of("1.txt", "2.csv", "3.xml"))
@@ -81,7 +81,7 @@ public class FileSearchTest {
         chainedFilter.add(new ExtensionFileFilter("csv"));
         chainedFilter.add(new ExtensionFileFilter("txt"));
 
-        result = new FileSearch().files(path, chainedFilter);
+        result = new FileSearch().files(SYSTEM_TMP, chainedFilter);
         assertThat(
                 result.stream().map(File::getName).collect(Collectors.toList()),
                 is(List.of("1.txt", "2.csv", "3.xml"))
