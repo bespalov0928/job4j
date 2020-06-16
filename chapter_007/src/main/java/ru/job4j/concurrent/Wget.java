b511d0dc4976c3696fcfdede3d140cf4f923ee6b
Dmitry Pratsun <pratsundv@gmail.com>
2020-06-16 17:22:40 +0300

    Прерывание блокированной нити.[267413#305230]

--- chapter_007/src/main/java/ru/job4j/concurrent/Wget.java
+++ chapter_007/src/main/java/ru/job4j/concurrent/Wget.java
@@ -3,13 +3,13 @@ package ru.job4j.concurrent;
 public class Wget {
     public static void main(String[] args) {
         new Thread(() -> {
-            try {
-                for (int i = 0; i < 100; i++) {
+            for (int i = 0; i < 100; i++) {
+                try {
                     System.out.print("\rLoading: " + i + "%");
                     Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
                 }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
             }
         }).start();
     }
