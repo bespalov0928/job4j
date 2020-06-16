c7c63f2d27fb7de0b4b8d77cb01e314609c7b5e6
Dmitry Pratsun <pratsundv@gmail.com>
2020-06-16 16:17:37 +0300

    Состояние нити.[229175#305228]

---
+++ chapter_007/src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,26 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    private static void printInfo(Thread thread) {
+        System.out.println(thread.getName() + " " + thread.getState());
+    }
+
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> { }
+        );
+        Thread second = new Thread(
+                () -> { }
+        );
+        printInfo(first);
+        printInfo(second);
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
+            printInfo(first);
+            printInfo(second);
+        }
+        printInfo(first);
+        printInfo(second);
+    }
+}
