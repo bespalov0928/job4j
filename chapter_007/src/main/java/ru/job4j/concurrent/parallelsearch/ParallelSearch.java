33afa170bfd4da8faaa194ae082125e898df8d2a
Dmitry Pratsun <pratsundv@gmail.com>
2020-06-27 18:35:08 +0300

    Обеспечить остановку потребителя.[66825#305222]

---
+++ chapter_007/src/main/java/ru/job4j/concurrent/parallelsearch/ParallelSearch.java
@@ -0,0 +1,36 @@
+package ru.job4j.concurrent.parallelsearch;
+
+import ru.job4j.concurrent.blockingqueue.SimpleBlockingQueue;
+
+public class ParallelSearch {
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    consumer.interrupt();
+                }
+
+        ).start();
+        consumer.start();
+    }
+}
