208a6d640072469b99ef4cb5d83ed4a80289f9e0
Dmitry Pratsun <pratsundv@gmail.com>
2020-06-30 15:19:44 +0300

    CAS - операции[6859#305250]

---
+++ chapter_007/src/main/java/ru/job4j/concurrent/cascount/CASCount.java
@@ -0,0 +1,21 @@
+package ru.job4j.concurrent.cascount;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+@ThreadSafe
+public class CASCount {
+    private final AtomicReference<Integer> count = new AtomicReference<>(0);
+
+    public void increment() {
+        Integer value;
+        do {
+            value = count.get();
+        } while (!count.compareAndSet(value, value + 1));
+    }
+
+    public int get() {
+        return count.get();
+    }
+}
